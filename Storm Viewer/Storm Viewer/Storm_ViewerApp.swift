80b33d8d41dc138a964707faade240c9d64e60d3
Herebiondev <contact@herebiondev.com>
2022-05-02 15:44:01 +0200

    Initial commit

---
+++ Storm Viewer/Storm Viewer/Storm_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Storm_ViewerApp.swift
+//  Storm Viewer
+//
+//  Created by Herebiondev on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Storm_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
