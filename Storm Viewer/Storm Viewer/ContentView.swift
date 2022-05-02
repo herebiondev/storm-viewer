c8f31a23783e0aef2211595be6e386ab9656305b
Herebiondev <contact@herebiondev.com>
2022-05-02 15:59:01 +0200

    Sample code

--- Storm Viewer/Storm Viewer/ContentView.swift
+++ Storm Viewer/Storm Viewer/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HSplitView {
+            Text("Left")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Text("Right")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
 }
 
