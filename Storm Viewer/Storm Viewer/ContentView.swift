a93d59ae4e3ba94fe5dd00c75dd6d78868cf5523
Herebiondev <contact@herebiondev.com>
2022-05-03 14:52:48 +0200

    Finishing touches

--- Storm Viewer/Storm Viewer/ContentView.swift
+++ Storm Viewer/Storm Viewer/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedImage: Int?
+    
     var body: some View {
-        HSplitView {
-            Text("Left")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationView {
+            List(0..<10, selection: $selectedImage) { number in
+                Text("Storm \(number + 1)")
+            }
+            .frame(width: 150)
             
-            Text("Right")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            if let selectedImage = selectedImage {
+                Image(String(selectedImage))
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Text("Please select an image")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            
         }
+        .navigationTitle("Storm Viewer")
+        .frame(minWidth: 480, minHeight: 320)
     }
 }
 
