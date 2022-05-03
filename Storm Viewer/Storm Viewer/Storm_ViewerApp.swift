32e27bedc86679dda29bd5781287cb24ae3926b8
Herebiondev <contact@herebiondev.com>
2022-05-03 15:18:00 +0200

    Use our AppDelegate class for our AppKit application delegate

--- Storm Viewer/Storm Viewer/Storm_ViewerApp.swift
+++ Storm Viewer/Storm Viewer/Storm_ViewerApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct Storm_ViewerApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
