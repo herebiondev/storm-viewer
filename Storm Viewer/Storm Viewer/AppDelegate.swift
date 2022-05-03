48c5a79813b0fc0ef152833ea043fffa6d545c0c
Herebiondev <contact@herebiondev.com>
2022-05-03 15:08:18 +0200

    Initial commit

---
+++ Storm Viewer/Storm Viewer/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Storm Viewer
+//
+//  Created by Herebiondev on 3/5/22.
+//
+//  "Delegates" are AppKit's preferred way of responding to events.
+//  AppKit developers frequently work with a protocol called
+//  NSApplicationDelegate, wich provides support for handling app-wide
+//  events such as receiving files, creating the dock menu, and -importantly-
+//  deciding what to do when the last windows is closed.
+//
+//  We need to use AppKit instead of SwiftUI because this is another thing that's
+//  easy to do in AppKit, but currently impossible in pure SwiftUI, so we need to
+//  write some code to handle it.
+
+import Foundation
